957a7b831d0885c5f6a092016e1b1695965d0dfe
Alessandro Zanardi <alessandro@zanardi.org>
2015-02-16 22:12:59 +0100

    Improved interface, added command line support, updated docs

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # CSVdata
 
+[![Travis](https://img.shields.io/travis/joyent/node.svg)](https://travis-ci.org/pensierinmusica/csvdata)
+[![David](https://img.shields.io/david/pensierinmusica/csvdata.svg)](https://www.npmjs.com/package/csvdata)
+[![npm](https://img.shields.io/npm/v/csvdata.svg)](https://www.npmjs.com/package/csvdata)
+
 ## Introduction
 
 CSVdata is a [npm](http://npmjs.org) module for [NodeJS](http://nodejs.org/), that loads and writes data operating with CSV files. Based on [node-csv](http://github.com/wdavidw/node-csv) and [q](http://github.com/kriskowal/q), supports promises and streams.
 
-It includes some smart checks to try preventing common errors that could compromise data integrity of the generated CSV (e.g. mixing of values due to an empty entry).
+It includes some smart checks to try preventing common errors that could compromise data integrity operating with CSV (e.g. mixing of values due to an empty entry).
 
 ## Install
 
@@ -16,15 +20,56 @@ It includes some smart checks to try preventing common errors that could comprom
 
 ## API
 
-##### Load
-`csvdata.load(path, [objname])`
+#### Load
+`csvdata.load(filepath, [options])`
 
-Returns an array where each item is an object containing data from a row. If "objname" is provided instead it will return an "index" object, with keys based on entries from the column matching the provided string, and values containing in turn an object with data for that entry.
+Reads data from "filepath" (the first line of the CSV file must contain headers).
 
-##### Write
-`csvdata.write(path, data, [header])`
+Returns a promise, eventually fulfilled with an array where each item is an object that contains data from a row (automatically parses native JS data types).
 
-Writes data to a file, indicated in "path". Data can be provided as:
+The **"options"** argument is a configuration object  with the following default values.
+
+```javascript
+{
+  objname: false;
+  stream: false;
+}
+```
+
+If "objname" is provided instead (string), the promise will be fulfilled with an "index" object, where keys are based on entries from the column matching "objname", and values contain in turn an object with data from that row (meant to be used when entries in the column "objname" are unique, and faster retrieval is convenient).
+
+If stream is set to `true`, it returns a [readable stream](http://nodejs.org/api/stream.html#stream_class_stream_readable) that can be piped where needed.
+
+```javascript
+// Imagine the CSV file content is:
+// name,hair,age
+// John,brown,36
+// Laura,red,23
+// Boris,blonde,28
+//
+
+csvdata.load('./my-file.csv')
+// -> Returns a promise that will be fulfilled with:
+// [
+//   {name: 'John', hair: 'brown', age: 36},
+//   {name: 'Laura', hair: 'red', age: 23},
+//   {name: 'Boris', hair: 'blonde', age: 28}
+// ]
+
+csvdata.load('./my-file.csv', {objname: 'name'})
+// -> Returns a promise that will be fulfilled with:
+{
+//   John: {name: 'John', hair: 'brown', age: 36},
+//   Laura: {name: 'Laura', hair: 'red', age: 23},
+//   Boris: {name: 'Boris', hair: 'blonde', age: 28}
+// }
+
+```
+
+#### Write
+`csvdata.write(filepath, data, [header])`
+
+Returns a promise, eventually fulfilled when done writing data to "filepath" (be careful, as it overwrites existing files). Data can be provided as:
 
  - String (e.g. `'a,b,c\nd,e,f'`)
  - Array of arrays (e.g. `[['a','b','c'],['d','e','f']]`)
@@ -32,6 +77,74 @@ Writes data to a file, indicated in "path". Data can be provided as:
  - Object containing objects (e.g. `{John: {amount: '100', name: 'John' }, Paul: {amount: '130', name: 'Paul'}}`).
 
 If "header" is provided (must be a string), it's written on the first line. If data comes from an object (i.e. last two cases above), "header" **must** be provided to guarantee the correct order of comma separated values, and can be used to **select** which object properties are saved to CSV.
+
+```javascript
+var data = [
+  {name: 'John', hair: 'brown', age: 36},
+  {name: 'Laura', hair: 'red', age: 23},
+  {name: 'Boris', hair: 'blonde', age: 28}
+];
+
+csvdata.write('./my-file.csv', data, 'name,hair,age')
+// Generates "my-file.csv" with this content:
+// name,hair,age
+// John,brown,36
+// Laura,red,23
+// Boris,blonde,28
+//
+
+csvdata.write('./my-file.csv', data, 'age,name,hair')
+// Generates "my-file.csv" with this content:
+// age,name,hair
+// 36,John,brown
+// 23,Laura,red
+// 28,Boris,blonde
+//
+```
+
+#### Check
+`csvdata.check(filepath, [options])`
+
+Checks data integrity for the CSV file indicated in "filepath". Returns a promise, eventually fulfilled with `true` if file is ok, or `false` if there are any problems. Specifically, it checks that every property defined in the header of the CSV file has a corresponding value for each row, and logs missing values if found (before using this method, make sure the first line of your CSV file is correct).
+
+The configuration object "options" has the default value: `{empty: true}`. If "empty" is set to `false` the "check" method considers empty values fine, but still complains for missing values.
+
+```javascript
+// Imagine the CSV file content is:
+// name,hair,age
+// John,brown,36
+// Laura,red
+// Boris,,28
+//
+
+csvdata.check('./my-file.csv')
+// -> Returns a promise that will be fulfilled with "false".
+// (also logs) 
+// - Wrong values on line 3
+// - Empty values on line 4
+
+csvdata.check('./my-file.csv', {empty: true})
+// -> Returns a promise that will be fulfilled with "false".
+// (also logs) 
+// - Wrong values on line 3
+```
+The "check" method can also be executed from the command line.
+
+```sh
+# You can run it either either as
+node csvdata.js -c <your_file_path.csv>
+
+# Or make the file executable with
+chmod +x csvdata.js
+
+# And then run it as
+./csvdata.js -c <your_file_path.csv>
+
+# (to accept empty values add the flag "-e").
+
+# For command line help
+node csvdata.js -h
+```
 
 ***
 
--- csvdata.js
+++ csvdata.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 'use strict';
 
 var fs = require('fs');
@@ -6,133 +8,219 @@ var csv = require('csv');
 var colors = require('colors');
 
 module.exports = {
-  load: function (path, objname) {
-    console.log('Reading data from '.yellow + path);
-    return Q.nfcall(fs.readFile, path, {encoding: 'utf8'}).then(function (data) {
-      if (data) {
-        console.log('Parsing data...'.yellow);
-        var parseOpts = {
-          auto_parse: true,
-          columns: true,
-          objname: objname || false,
-          skip_empty_lines: true
+  load: function (path, opts) {
+    opts = opts || {stream: false};
+    var parseOpts = {
+      auto_parse: true,
+      columns: true,
+      objname: opts.objname || false,
+      skip_empty_lines: true
+    };
+    console.log('\nReading data from ' + path + '\n');
+    if (opts.stream) {
+      return fs.createReadStream(path, {encoding: 'utf8'}).pipe(csv.parse(parseOpts));
+    } else {
+      return Q.nfcall(fs.readFile, path, {encoding: 'utf8'}).then(function (data) {
+        if (data) {
+          console.log('Parsing data...\n'.yellow);
+          return Q.nfcall(csv.parse, data, parseOpts).then(function (data) {
+            console.log('Data parsed\n'.green);
+            return data;
+          });
+        } else {
+          console.log('File appears to be empty!\n'.yellow);
         }
-        return Q.nfcall(csv.parse, data, parseOpts).then(function (data) {
-          console.log('Data parsed'.green);
-          return data
-        })
-      } else {
-        console.log('File appears to be empty!'.yellow);
-      }
-    })
+      });
+    }
   },
   write: function (path, data, header) {
-    var ws = fs.createWriteStream(path);
-    console.log(('Writing data to ' + path).yellow);
-    var hlen; // Used to compute number of CSV columns
-    if (header) {
-      if (typeof header === 'string') {
-        ws.write(header + '\n');
-        header = header.split(',');
-        hlen = header.length;
-      } else {
-        throw new Error('The header argument must be a string'.red);
-      }
-    }
-    if (typeof data === 'string' || Array.isArray(data)) {
-      if (typeof data === 'string') {
-        data = data.split('\n');
-        data.forEach(function (item, index) {
-          data[index] = item.split(',');
+    return Q.promise(function (resolve, reject) {
+      var ws = fs.createWriteStream(path, {encoding: 'utf8'});
+      ws
+        .on('finish', function () {
+          resolve();
+        })
+        .on('error', function (err) {
+          reject(err);
         });
+      console.log(('\nWriting data to ' + path + '\n'));
+      var hlen, i, len, entry, obj;
+      if (header) {
+        if (typeof header === 'string') {
+          ws.write(header + '\n');
+          header = header.split(',');
+          hlen = header.length;
+        } else {
+          throw new Error('The header argument must be a string'.red);
+        }
       }
-      // It's an array (of arrays or of objects?)
-      if (Array.isArray(data[0])) {
-        // It's an array of arrays
-        hlen = hlen || data[0].length;
-        for (var i = 0, len = data.length; i < len; i++) {
-          var arr = data[i];
-          if (Array.isArray(arr)) {
-            if (arr.length === hlen) {
-              ws.write(arr.join(',') + '\n');
+      if (typeof data === 'string' || Array.isArray(data)) {
+        if (typeof data === 'string') {
+          data = data.split('\n');
+          data.forEach(function (item, index) {
+            data[index] = item.split(',');
+          });
+        }
+        // It's an array (of arrays or of objects?)
+        if (Array.isArray(data[0])) {
+          // It's an array of arrays
+          hlen = hlen || data[0].length;
+          for (i = 0, len = data.length; i < len; i++) {
+            var arr = data[i];
+            if (Array.isArray(arr)) {
+              if (arr.length === hlen) {
+                ws.write(arr.join(',') + '\n');
+              } else {
+                if (arr[0] !== '' || arr.length !== 1) {
+                  throw new Error(('Number of values at row ' + i + ' different from first line of CSV\n').red +
+                                  'First line length: ' + hlen + '\n' +
+                                  'Row  (length ' + arr.length + '): ' + JSON.stringify(arr) + '\n');
+                }
+              }
             } else {
-              throw new Error(('Number of values at row ' + i + ' different from first line of CSV\n').red +
-                              'First line length: ' + hlen + '\n' +
-                              'Row  (length ' + arr.length + '): ' + JSON.stringify(arr) + '\n');
-              break;
+              throw new Error(('Wrong input in array at index ' + i + '\n').red +
+                              'This item is not an array:\n' + JSON.stringify(arr) + '\n');
+            }
+          }
+        } else if (typeof data[0] === 'object') {
+          // It's an array of objects
+          if (header) {
+            for (i = 0, len = data.length; i < len; i++) {
+              obj = data[i];
+              if (typeof obj === 'object') {
+                entry = [];
+                for (var j = 0; j < hlen; j++) {
+                  if (obj.hasOwnProperty(header[j])) {
+                    entry.push(obj[header[j]]);
+                  } else {
+                    throw new Error('Object properties do not conform to the format specified in header\n'.red +
+                                    'Header: ' + JSON.stringify(header) + '\n' +
+                                    'Object: ' + JSON.stringify(obj) + '\n');
+                  }
+                }
+                ws.write(entry.join(',') + '\n');
+              } else {
+                throw new Error(('Wrong input in array at index ' + i + '\n').red +
+                                'This item is not an object:\n' + JSON.stringify(obj) + '\n');
+              }
             }
           } else {
-            throw new Error(('Wrong input in array at index ' + i + '\n').red +
-                            'This item is not an array:\n' + JSON.stringify(arr) + '\n');
-            break;
+            throw new Error('When data comes from an object, the header argument must be provided\n'.red);
           }
         }
-      } else if (typeof data[0] === 'object') {
-        // It's an array of objects
+      } else if (typeof data === 'object') {
+        // It's an object (containing objects?)
         if (header) {
-          for (var i = 0, len = data.length; i < len; i++) {
-            var obj = data[i];
-            if (typeof obj === 'object') {
-              var entry = [];
-              for (var j = 0; j < hlen; j++) {
-                if (obj.hasOwnProperty(header[j])) {
-                  entry.push(obj[header[j]]);
+          for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+              entry = [];
+              obj = data[key];
+              for (i = 0; i < hlen; i++) {
+                if (obj.hasOwnProperty(header[i])) {
+                  entry.push(obj[header[i]]);
                 } else {
                   throw new Error('Object properties do not conform to the format specified in header\n'.red +
                                   'Header: ' + JSON.stringify(header) + '\n' +
                                   'Object: ' + JSON.stringify(obj) + '\n');
-                  break;
                 }
               }
               ws.write(entry.join(',') + '\n');
-            } else {
-              throw new Error(('Wrong input in array at index ' + i + '\n').red +
-                              'This item is not an object:\n' + JSON.stringify(obj) + '\n');
-              break;
             }
           }
         } else {
           throw new Error('When data comes from an object, the header argument must be provided\n'.red);
         }
+      } else {
+        throw new Error('Wrong input!'.red + ' Data can be accepted only in these formats:\n' +
+                        '- String\n' +
+                        '- Array of arrays\n' +
+                        '- Array of objects\n' +
+                        '- Object containing objects\n' +
+                        '(see documentation for further details)\n');
       }
-    } else if (typeof data === 'object') {
-      // It's an object (containing objects?)
-      if (header) {
-        var flag = true;
-        for (var key in data) {
-          if (flag) {
-            if (data.hasOwnProperty(key)) {
-              var entry = [];
-              var obj = data[key];
-              for (var i = 0; i < hlen; i++) {
-                if (obj.hasOwnProperty(header[i])) {
-                  entry.push(obj[header[i]]);
-                } else {
-                  flag = false;
-                  throw new Error('Object properties do not conform to the format specified in header\n'.red +
-                                  'Header: ' + JSON.stringify(header) + '\n' +
-                                  'Object: ' + JSON.stringify(obj) + '\n');
-                  break;
+      ws.end();
+      console.log('Data written!\n'.green);
+    });
+  },
+  check: function (path, opts) {
+    opts = opts || {empty: true};
+    var result = true;
+    var hlen;
+    var count = 1;
+    console.log('\nReading data from ' + path + '\n');
+    return Q.nfcall(fs.readFile, path, {encoding: 'utf8'}).then(function (data) {
+      if (data) {
+        var parseOpts = {
+          skip_empty_lines: true
+        };
+        return Q.nfcall(csv.parse, data, parseOpts).then(function (data) {
+          console.log('Checking data...\n'.yellow);
+          hlen = data[0].length;
+          return Q.promise(function (resolve, reject) {
+            csv.transform(data, function (item) {
+              if (item.length !== hlen) {
+                console.error('- Wrong values on line ' + count);
+                result = false;
+              } else {
+                if (opts.empty) {
+                  item.forEach(function (value) {
+                    if (!value) {
+                      result = false;
+                      console.error('- Empty values on line ' + count);
+                    }
+                  });
                 }
               }
-              ws.write(entry.join(',') + '\n');
-            }
-          } else {
-            break;
+              count++;
+            }, function (err) {
+              if (err) {
+                if (program.check) {
+                  console.error(err);
+                  console.log('\n');
+                }
+                reject(err);
+              }
+              else {
+                if (result) {
+                  console.log('\nFile looks ok!\n'.green);
+                } else {
+                  console.error('\nFile has problems!\n'.red);
+                }
+                resolve(result);
+              }
+            });
+          });
+        }, function (err) {
+          if (program.check && err) {
+            console.error(err);
+            console.log('\n');
           }
-        }
+        });
       } else {
-        throw new Error('When data comes from an object, the header argument must be provided\n'.red);
+        console.log('File appears to be empty!\n'.yellow);
       }
-    } else {
-      throw new Error('Wrong input!'.red + ' Data can be accepted only in these formats:\n\
-                      - String\n\
-                      - Array of arrays\n\
-                      - Array of objects\n\
-                      - Object containing objects\n\
-                      (see documentation for further details)\n');
-    }
-    ws.end();
-    console.log('Data written'.green);
+    }, function (err) {
+      if (program.check && err) {
+        console.error(err);
+        console.log('\n');
+      }
+    });
   }
 };
+
+// Code for command line usage of the "check" method
+if (process.argv[1].match(/csvdata/)) {
+
+  var program = require('commander');
+
+  program
+    .usage('[options] <file ...>')
+    .option('-c, --check [filepath]', 'Check data integrity for the CSV file indicated at [filepath]')
+    .option('-e, --empty', 'Accept empty values')
+    .parse(process.argv);
+
+  if (program.check) {
+    module.exports.check(program.check, {empty: !program.empty});
+  }
+
+}
